e8b9d6ac835ca7eebcc1ab881a642e48536188cc
seunshix <adexseun13@Gmail.com>
2023-04-26 11:54:21 -0700

    task 3: write unit tests for the refactored system

---
+++ test/test_battery/test_nubbin_battery.py
@@ -0,0 +1,10 @@
+import unittest
+from datetime import date
+from battery.battery_type.nubbin_battery import NubbinBattery
+
+class TestNubbinBattery(unittest.TestCase):
+    def test_needs_service_true(self):
+        pass
+
+    def test_needs_service_false(self):
+        pass
